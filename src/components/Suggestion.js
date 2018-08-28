8c0fa4132ae7c1fc21bed3d4b12b003948698edc
Anton Bukin (InPlat) <abukin@megalabs.ru>
2018-08-28 16:28:34 +0300

    Добавлен скрол в сафари и исправлен выбор значения

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -21,6 +21,7 @@ class Suggestion extends Component {
         onMouseEnter={ this.handleHover }
         onMouseLeave={ this.handleBlur }
         onMouseDown={ this.props.onSelect }
+        onClick={ this.props.onSelect }
       >
         <SuggestionValue
           {...{ highlighting, searchWords, value }}
